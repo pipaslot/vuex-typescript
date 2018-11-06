fd2f739da54207eedb97ba9530e5d69c321b6299
Petr Stipek <stipek@cngroup.dk>
2018-11-06 22:19:19 +0100

    SImplified example and added module

--- examples/basic/src/App.vue
+++ examples/basic/src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <Counter/>
+    Delta: <input type="number" v-model.number="number">
+    <h3>Root Store</h3>
+    <dl>
+      <dt>Action:</dt>
+      <dd>
+        <button @click="incrementRoot">+</button>
+        <button @click="decrementRoot">-</button>
+      </dd>
+      <dt>State: </dt>
+      <dd>{{asRootState}}</dd>
+      <dt>Getter: </dt>
+      <dd>{{asRootGetter}}</dd>
+    </dl>
+    <h3>Sync Module</h3>
+    <dl>
+      <dt>Action:</dt>
+      <dd>
+        <button @click="incrementSync">+</button>
+        <button @click="decrementSync">-</button>
+      </dd>
+      <dt>State: </dt>
+      <dd>{{asSyncState}}</dd>
+      <dt>Getter: </dt>
+      <dd>{{asSyncGetter}}</dd>
+    </dl>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Counter from './components/Counter.vue';
+import { Component, Vue } from "vue-property-decorator";
 
-@Component({
-  components: {
-    Counter,
-  },
-})
-export default class App extends Vue {}
+@Component
+export default class App extends Vue {
+  number: number = 1;
+  get asRootGetter() {
+    return this.$store.asString;
+  }
+  get asRootState() {
+    return this.$store.state.count;
+  }
+  incrementRoot() {
+    this.$store.increment(this.number);
+  }
+  decrementRoot() {
+    this.$store.increment(-this.number);
+  }
+
+  get asSyncGetter() {
+    return this.$store.syncModule.asString;
+  }
+  get asSyncState() {
+    return this.$store.syncModule.state.count;
+  }
+  incrementSync() {
+    this.$store.syncModule.increment(this.number);
+  }
+  decrementSync() {
+    this.$store.syncModule.increment(-this.number);
+  }
+}
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  width:300px;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto 0 auto;
+}
+dl {
+  text-align: left;
 }
 </style>
--- examples/basic/src/store/syncModule.ts
+++ examples/basic/src/store/syncModule.ts
@@ -1,16 +1,16 @@
 import { Mutations, Store } from "../../../../src";
-import syncModule from "./syncModule";
-export class RootState {
+
+class SyncState {
   count: number = 0;
 }
 
-export class RootMutations extends Mutations<RootState> {
+class SyncMutations extends Mutations<SyncState> {
   setCount(input: number) {
     this.state.count = input;
   }
 }
 
-export class MyStore extends Store<RootState, RootMutations> {
+class SyncModule extends Store<SyncState, SyncMutations> {
   // Define store action
   increment(input: number) {
     this.mutations.setCount(this.state.count + input);
@@ -19,7 +19,6 @@ export class MyStore extends Store<RootState, RootMutations> {
   get asString() {
     return "Count: " + this.state.count + "!!!";
   }
-
-  syncModule = syncModule;
 }
-export default new MyStore(new RootState(), new RootMutations());
+
+export default new SyncModule(new SyncState(), new SyncMutations());
