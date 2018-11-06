83725ddee40d01c558c91df56cb08309ef08929f
Petr Stipek <stipek@cngroup.dk>
2018-11-06 19:14:04 +0100

    refactoring

--- src/devtoolPlugin.ts
+++ src/devtoolPlugin.ts
@@ -1,15 +1,17 @@
-export default function devtoolPlugin(store:any) {
+import { IStoreProxy } from "./types";
+
+export default function devtoolPlugin(store: IStoreProxy<any>) {
   const devtoolHook: any =
     typeof window !== "undefined" && (<any>window).__VUE_DEVTOOLS_GLOBAL_HOOK__;
   if (!devtoolHook) return;
 
   devtoolHook.emit("vuex:init", store);
 
-  devtoolHook.on("vuex:travel-to-state", (targetState:any) => {
+  devtoolHook.on("vuex:travel-to-state", (targetState: any) => {
     store.replaceState(targetState);
   });
 
-  store.subscribe((mutation:any, state:any) => {
+  store.subscribe((mutation: any, state: any) => {
     devtoolHook.emit("vuex:mutation", mutation, state);
   });
 }
