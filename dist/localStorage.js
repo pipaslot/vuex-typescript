71fa009127c4c52a14bd6c031f740648cb2be564
Petr Stipek <stipek@cngroup.dk>
2019-03-24 09:52:38 +0100

    updated build

--- dist/localStorage.js
+++ dist/localStorage.js
@@ -1,3 +1,13 @@
+var reISO = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
+var dateParser = function (key, value) {
+    if (typeof value === "string") {
+        var a = reISO.exec(value);
+        if (a) {
+            return new Date(value);
+        }
+    }
+    return value;
+};
 var LocalStorage = /** @class */ (function () {
     function LocalStorage(_prefix) {
         if (_prefix === void 0) { _prefix = ""; }
@@ -10,7 +20,7 @@ var LocalStorage = /** @class */ (function () {
         var value = localStorage.getItem(this._prefix + key);
         if (value) {
             try {
-                return JSON.parse(value);
+                return JSON.parse(value, dateParser);
             }
             catch (e) {
                 return null;
